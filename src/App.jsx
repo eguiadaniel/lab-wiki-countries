d9395c423033eacaaf2d6ffd7f888bb2f8a6e427
fnwebdev <publicidad@fantasynails.es>
2021-03-15 12:46:58 +0100

    Flex nowrap for long names

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter>
       <Navbar></Navbar>
       <div className="container">
-      <div className="row">
+      <div className="row" style={{flexWrap: 'nowrap'}}>
           <CountriesList countries={countries} />
           
           <Switch>
